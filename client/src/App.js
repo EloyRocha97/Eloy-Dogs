460cd9a29331dbbda0f80e5149999fa8da49f57a
EloyRocha97 <rochaeloy11@gmail.com>
2023-08-07 11:57:53 -0300

    link

--- client/src/App.js
+++ client/src/App.js
@@ -3,47 +3,37 @@ import { Route, useLocation } from "react-router-dom";
 import { Home, Landing, Form, Detail } from "./Views";
 import NavBar from "./Components/NavBar/navBar";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://eloy-dogs.onrender.com";
 
 function App() {
-	// const location = useLocation;
+  // const location = useLocation;
 
-	return (
-		<div className="App">
-			{/* {location.pathname !== "/" && <NavBar />} */}
-			<Route
-				path={[
-					"/home",
-					"/recipes",
-					"/about",
-					"/addrecipes",
-					`/dogs`,
-					"/create",
-				]}
-				component={NavBar}
-			/>
-			<Route
-				exact
-				path="/"
-				component={Landing}
-			/>
+  return (
+    <div className="App">
+      {/* {location.pathname !== "/" && <NavBar />} */}
+      <Route
+        path={[
+          "/home",
+          "/recipes",
+          "/about",
+          "/addrecipes",
+          `/dogs`,
+          "/create",
+        ]}
+        component={NavBar}
+      />
+      <Route exact path="/" component={Landing} />
 
-			<Route
-				path="/home"
-				render={() => <Home />}
-			/>
+      <Route path="/home" render={() => <Home />} />
 
-			<Route
-				exact
-				path="/dogs/:id"
-				render={({ match }) => <Detail id={match.params.id} />}
-			/>
-			<Route
-				path="/create"
-				render={() => <Form />}
-			/>
-		</div>
-	);
+      <Route
+        exact
+        path="/dogs/:id"
+        render={({ match }) => <Detail id={match.params.id} />}
+      />
+      <Route path="/create" render={() => <Form />} />
+    </div>
+  );
 }
 
 export default App;

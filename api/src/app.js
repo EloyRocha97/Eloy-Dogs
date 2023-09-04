a26be8a4c368cebcfa889ebd156b483574048e93
EloyRocha97 <rochaeloy11@gmail.com>
2023-09-04 14:39:32 -0300

    a

--- api/src/app.js
+++ api/src/app.js
@@ -11,7 +11,12 @@ const server = express();
 
 server.name = "API";
 
-server.use(cors());
+server.use(
+  cors({
+    origin: ["https://eloy-dogs-front.onrender.com"],
+    credentials: true,
+  })
+);
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());

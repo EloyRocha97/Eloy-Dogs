fd42046a2a5132ae3c23d60ee07be994b0685f75
EloyRocha97 <rochaeloy11@gmail.com>
2023-09-04 13:11:50 -0300

    dep?

--- api/src/app.js
+++ api/src/app.js
@@ -18,7 +18,7 @@ server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
-    "https://eloy-dogs.onrender.com",
+    "https://eloy-dogs-front.onrender.com",
     "Access-Control-Allow-Origin",
     "http://localhost:3000"
   ); // update to match the domain you will make the request from
